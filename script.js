16a05da7add3ab36b66617828e246de888c8dc90
fbelam <36649120+fbelam@users.noreply.github.com>
2023-10-02 22:26:48 -0300

    meses ....

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const calendar = document.querySelector(".calendar");
     date = document.querySelector(".date");
     daysContainer = document.querySelector(".days");
     prev = document.querySelector(".prev");
-    next = document.querySelector(".next");
+    next = document.querySelector(".next"),
+    todayBtn = document.querySelector(".today-btn"),
+    gotoBtn = document.querySelector(".goto-btn"),
+    dateInput = document.querySelector(".date-input");
 
 let today = new Date();
 let activeDay;
-let day = today.getDay();
 let month = today.getMonth();
 let year = today.getFullYear();
 
@@ -38,7 +40,7 @@ function initCalendar() {
     const prevDays = prevLastDay.getDate();
     const lastDate = lastDay.getDate();
     const day = firstDay.getDay();
-    const nextDays = 1+lastDay.getDay();
+    const nextDays = 7 - lastDay.getDay() - 1;
 
    console.log(nextDays)
 //atualizar data no topo do calendário
@@ -76,11 +78,56 @@ date.innerHTML = months[month] + " " + year;
         //dias do proximo mes 
 
         for (let j = 1; j <= nextDays; j++) {
-            days += `<div class="day next-date">${j}</div>`;
+            days += `<div class="day next-date ">${j}</div>`;
         }
 
     daysContainer.innerHTML = days;     
 }
 
 
+
+function prevMonth() {
+    month --;
+    if (month <0 ) {
+        month = 11;
+        year --;
+    }
+    initCalendar();
+}
+
+//next month
+
+function nextMonth() {
+    month ++;
+    if (month>11) {
+        month = 0;
+        year ++;
+}
+    initCalendar();
+}
+
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
 initCalendar();
+
+todayBtn.addEventListener("click", () =>{
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+})
+
+dateInput.addEventListener("input", (e) => {
+    //permitir apenas números remover qualquer outra coisa
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    if (dateInput.value.length === 2) {
+    //adicione uma barra de dois números inseridos
+    dateInput.value += "/";
+    }
+    if (dateInput.value.length > 7) {
+        //não permitir mais de 7 caracteres
+        dateInput.value = dateInput.value.slice(0, 7);
+    }
+})
